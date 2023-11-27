b7bb2c26bf50536c36f6962181a401ddfcfc846b
achwek <fhaielachwek01@gmail.com>
2023-11-27 13:42:46 +0100

    Creation of MixController class

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VinylMixRepository;
 use function Symfony\Component\String\u;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
