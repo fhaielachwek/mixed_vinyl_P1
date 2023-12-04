d9ec1011ec03842f10aa248afdca90f2c5728759
achwek <fhaielachwek01@gmail.com>
2023-12-04 12:08:33 +0100

    Querying for a Single Entity for a Show Page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -19,9 +19,10 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}, name: 'app_mix_show'')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-        dd($id);
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
